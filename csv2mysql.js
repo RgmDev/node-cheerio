3ddecea0d473badcb8882d4d319f0ca202bd6bd1
Rubén Gonzaléz <rgonzalez@MKD.car>
2020-01-20 18:06:21 +0100

    Integracion con MySQL

--- csv2mysql.js
+++ csv2mysql.js
@@ -13,25 +13,31 @@ con.connect(function(err) {
   console.log("Connected!")
 });
 
-/*
+
 fs.readFile('data.csv', 'utf-8', (err, data) => {
   if(err){
     console.log('error: ', err)
   }else{
     console.log('Lectura del archivo correcta')
     let file = data.split("\n")
-    
     file.forEach(function(item){
-      console.log(item+"Linea")
+      if(item != ''){
+        // console.log(item)
+        let cols = item.split(';')
+        if(cols[7] == 'NaN'){cols[7]=null}
+        if(cols[5] == 'NaN'){cols[5]=null}
+        let sql = 'INSERT INTO jornadas values (\''+cols[0]+'\',\''+cols[1]+'\','+cols[2]+','+cols[3]+',\''+cols[4]+'\','+cols[5]+',\''+cols[6]+'\','+cols[7]+',\''+cols[8]+'\')'
+        console.log(sql)
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          // console.log("1 record inserted")
+        })
+        
+      }
+
     })
-    
   }
-});
-*/
+})
 
-
-
-
-
-con.end()
+// con.end()
 
